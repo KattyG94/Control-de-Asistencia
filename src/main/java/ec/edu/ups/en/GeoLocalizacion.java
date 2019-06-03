db7399f7138f30ade5bd6dda51ea99bb000afa67
Angel <angelpaqui36@gmail.com>
2019-06-03 16:09:06 -0500

    Facelets

--- src/main/java/ec/edu/ups/en/GeoLocalizacion.java
+++ src/main/java/ec/edu/ups/en/GeoLocalizacion.java
@@ -1,5 +1,47 @@
 package ec.edu.ups.en;
 
+import java.util.Date;
+
 public class GeoLocalizacion {
+	private int id;
+	private Double latitud;
+	private Double longitud;
+	private Date fecha;
+
+	public GeoLocalizacion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(Double latitud) {
+		this.latitud = latitud;
+	}
+
+	public Double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(Double longitud) {
+		this.longitud = longitud;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
 }
